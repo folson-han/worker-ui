d2068e42c5a9d077c42e8c1478d3182a8b353b87
chen <ch.10234en@gmail.com>
2021-10-14 18:46:36 +0800

    :tada: first commit

---
+++ packages/Button/index.js
@@ -0,0 +1,7 @@
+import WButton from "./src"
+
+WButton.install = function (Vue) {
+    Vue.component(WButton.name, WButton);
+}
+
+export default WButton;
---
+++ packages/Button/src/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="w-button">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "w-button",
+  props: {
+    type: {
+      type: String,
+      default: "default",
+    },
+
+  }
+})
+</script>
